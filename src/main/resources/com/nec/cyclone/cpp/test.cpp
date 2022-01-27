6d1321382316a4a052899e98f0d6ff4b2533d618
Benson Ma <q10@users.noreply.github.com>
2022-01-26 21:28:58 -0800

    c++ wip

--- src/main/resources/com/nec/cyclone/cpp/test.cpp
+++ src/main/resources/com/nec/cyclone/cpp/test.cpp
@@ -90,10 +90,12 @@ nullable_varchar_vector * project_eval(const nullable_varchar_vector *input_0)  
   frovedis::words output_0_input_words = varchar_vector_to_words(input_0);
   std::vector<size_t> output_0_starts(input_0->count);
   std::vector<size_t> output_0_lens(input_0->count);
+
   for ( int32_t i = 0; i < input_0->count; i++ ) {
     output_0_starts[i] = output_0_input_words.starts[i];
     output_0_lens[i] = output_0_input_words.lens[i];
   }
+
   std::vector<size_t> output_0_new_starts;
   std::vector<int> output_0_new_chars = frovedis::concat_words(
     output_0_input_words.chars,
@@ -102,6 +104,7 @@ nullable_varchar_vector * project_eval(const nullable_varchar_vector *input_0)  
     "",
     (std::vector<size_t>&)(output_0_new_starts)
   );
+
   output_0_input_words.chars = output_0_new_chars;
   output_0_input_words.starts = output_0_new_starts;
   output_0_input_words.lens = output_0_lens;
@@ -155,7 +158,129 @@ void projection_test() {
   return;
 }
 
+void bucket_grouping_test_fail() {
+  std::vector<std::string> data { "AIR", "MAIL", "RAIL", "SHIP", "TRUCK", "REG AIR", "FOB" };
+  std::vector<size_t> id_to_bucket { 0, 1, 0, 1, 0, 1, 0 };
+
+  const auto *input = from_vec(data);
+  set_validity(input->validityBuffer, 3, 0);
+
+  frovedis::words output_0_input_words = varchar_vector_to_words(input);
+  auto * output_0 = new nullable_varchar_vector;
+
+  std::vector<size_t> output_0_starts(id_to_bucket.size());
+  std::vector<size_t> output_0_lens(id_to_bucket.size());
+
+  for (auto i = 0; i < id_to_bucket.size(); i++ ) {
+    if (id_to_bucket[i]) {
+      output_0_starts[i] = output_0_input_words.starts[i];
+      output_0_lens[i] = output_0_input_words.lens[i];
+    }
+  }
+
+  std::vector<size_t> output_0_new_starts;
+  std::vector<int> output_0_new_chars = frovedis::concat_words(
+    output_0_input_words.chars,
+    (const std::vector<size_t>&)(output_0_starts),
+    (const std::vector<size_t>&)(output_0_lens),
+    "",
+    (std::vector<size_t>&)(output_0_new_starts)
+  );
+
+  output_0_input_words.chars = output_0_new_chars;
+  output_0_input_words.starts = output_0_new_starts;
+  output_0_input_words.lens = output_0_lens;
+
+  words_to_varchar_vector(output_0_input_words, output_0);
+
+  auto o = 0;
+  for (auto i = 0; i < id_to_bucket.size(); i++ ) {
+    if (id_to_bucket[i]) {
+      set_validity(output_0->validityBuffer, o++, 1);
+    }
+  }
+
+  std::cout << "================================================================================" << std::endl;
+  std::cout << "BUCKET GROUPING TEST (FAILING)\n" << std::endl;
+
+  debug_nullable_varchar_vector(input);
+
+  std::cout << "ID_TO_BUCKET: [ ";
+  for (auto i = 0; i < id_to_bucket.size(); i++) {
+      std::cout << id_to_bucket[i] << ", ";
+  }
+  std::cout << "]\n\n";
+
+  debug_nullable_varchar_vector(output_0);
+
+  std::cout << "================================================================================" << std::endl;
+  return;
+}
+
+void bucket_grouping_test_pass() {
+  std::vector<std::string> data { "AIR", "MAIL", "RAIL", "SHIP", "TRUCK", "REG AIR", "FOB" };
+  std::vector<size_t> id_to_bucket { 0, 1, 0, 1, 0, 1, 0 };
+
+  const auto *input = from_vec(data);
+  set_validity(input->validityBuffer, 3, 0);
+
+  frovedis::words output_0_input_words = varchar_vector_to_words(input);
+  auto * output_0 = new nullable_varchar_vector;
+
+  std::vector<size_t> output_0_starts;
+  std::vector<size_t> output_0_lens;
+  output_0_starts.reserve(data.size());
+  output_0_lens.reserve(data.size());
+
+  for (auto i = 0; i < id_to_bucket.size(); i++ ) {
+    if (id_to_bucket[i]) {
+      output_0_starts.emplace_back(output_0_input_words.starts[i]);
+      output_0_lens.emplace_back(output_0_input_words.lens[i]);
+    }
+  }
+
+  std::vector<size_t> output_0_new_starts;
+  std::vector<int> output_0_new_chars = frovedis::concat_words(
+    output_0_input_words.chars,
+    output_0_starts,
+    output_0_lens,
+    "",
+    output_0_new_starts
+  );
+
+  output_0_input_words.chars = output_0_new_chars;
+  output_0_input_words.starts = output_0_new_starts;
+  output_0_input_words.lens = output_0_lens;
+
+  words_to_varchar_vector(output_0_input_words, output_0);
+
+  auto o = 0;
+  for (auto i = 0; i < id_to_bucket.size(); i++ ) {
+    if (id_to_bucket[i]) {
+      set_validity(output_0->validityBuffer, o++, check_valid(input->validityBuffer, i));
+    }
+  }
+
+  std::cout << "================================================================================" << std::endl;
+  std::cout << "BUCKET GROUPING TEST (PASSING)\n" << std::endl;
+
+  debug_nullable_varchar_vector(input);
+
+  std::cout << "ID_TO_BUCKET: [ ";
+  for (auto i = 0; i < id_to_bucket.size(); i++) {
+      std::cout << id_to_bucket[i] << ", ";
+  }
+  std::cout << "]\n\n";
+
+  debug_nullable_varchar_vector(output_0);
+
+  std::cout << "================================================================================" << std::endl;
+  return;
+}
+
 int main() {
-  projection_test();
-  filter_test();
+  // projection_test();
+  // filter_test();
+  bucket_grouping_test_fail();
+  bucket_grouping_test_pass();
 }
